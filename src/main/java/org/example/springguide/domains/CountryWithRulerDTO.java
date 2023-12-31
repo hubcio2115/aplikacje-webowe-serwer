09f314e9fca6bfddbfe63f65c09707a295fcf740
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-31 14:11:19 +0100

    added CountryWithRulerDTO

---
+++ src/main/java/org/example/springguide/domains/CountryWithRulerDTO.java
@@ -0,0 +1,14 @@
+package org.example.springguide.domains;
+
+import java.io.Serializable;
+import java.time.Year;
+import java.util.Optional;
+
+public record CountryWithRulerDTO(
+        String name,
+        long gdp,
+        boolean isInEurope,
+        Year formationYear,
+        Optional<RulerDTO> ruler
+) implements Serializable {
+}
