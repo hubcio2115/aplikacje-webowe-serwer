90990b6e3492f60e767d84d202ac34ba7a4d0ada
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-31 14:11:03 +0100

    added findByCountryId to RulerRepository

--- src/main/java/org/example/springguide/domains/RulerRepository.java
+++ src/main/java/org/example/springguide/domains/RulerRepository.java
@@ -2,5 +2,9 @@ package org.example.springguide.domains;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RulerRepository extends JpaRepository<Ruler, Long> {
+
+    Optional<Ruler> findByCountryId(long countryId);
 }
