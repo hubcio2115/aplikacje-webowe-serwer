40e87654b6e609d13fe3d627ead11bdd3910184b
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-02 17:57:45 +0100

    formatted Ruler Repository

--- src/main/java/org/example/server/domains/ruler/RulerRepository.java
+++ src/main/java/org/example/server/domains/ruler/RulerRepository.java
@@ -1,8 +1,7 @@
 package org.example.server.domains.ruler;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RulerRepository extends JpaRepository<Ruler, Long> {
   Optional<Ruler> findByCountryId(long countryId);
