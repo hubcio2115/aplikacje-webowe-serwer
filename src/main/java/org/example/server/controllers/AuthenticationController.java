eab070c3442d869fc747091d3836f994963c7f63
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-02 18:00:52 +0100

    added AuthenticationController and AuthenticationService

---
+++ src/main/java/org/example/server/controllers/AuthenticationController.java
@@ -0,0 +1,38 @@
+package org.example.server.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.example.server.auth.AuthenticationRequest;
+import org.example.server.auth.AuthenticationResponse;
+import org.example.server.auth.AuthenticationService;
+import org.example.server.auth.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationService service;
+
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+    return ResponseEntity.ok(service.register(request));
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+      @RequestBody AuthenticationRequest request) {
+    return ResponseEntity.ok(service.authenticate(request));
+  }
+
+  @PostMapping("/refresh-token")
+  public void refreshToken(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    service.refreshToken(request, response);
+  }
+}
