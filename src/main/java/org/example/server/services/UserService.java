78d76988eeb1c14d072f2f7981854e2acb8eb58b
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-02 17:59:25 +0100

    added changeDetails and deleted same password check

--- src/main/java/org/example/server/services/UserService.java
+++ src/main/java/org/example/server/services/UserService.java
@@ -1,15 +1,15 @@
 package org.example.server.services;
 
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.example.server.domains.user.ChangePasswordRequest;
+import org.example.server.domains.user.ChangeUserDetailsRequest;
 import org.example.server.domains.user.User;
 import org.example.server.domains.user.UserRepository;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import lombok.val;
-
-import java.security.Principal;
 
 @Service
 @RequiredArgsConstructor
@@ -19,20 +19,30 @@ public class UserService {
   private final UserRepository userRepository;
 
   public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
+
     val user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
 
-    // check if the current password is correct
-    if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword()))
-      throw new IllegalStateException("Wrong password");
-
     // check if the two new passwords are the same
-    if (!request.getNewPassword().equals(request.getConfirmationPassword()))
+    if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
       throw new IllegalStateException("Password are not the same");
+    }
 
     // update the password
     user.setPassword(passwordEncoder.encode(request.getNewPassword()));
 
     // save the new password
+    userRepository.save(user);
+  }
+
+  public void changeDetails(ChangeUserDetailsRequest request, Principal connectedUser) {
+    val user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+    // update the details
+    user.setFirstName(request.firstName());
+    user.setLastName(request.lastName());
+    user.setEmail(request.email());
+
+    // save the new details
     userRepository.save(user);
   }
 }
