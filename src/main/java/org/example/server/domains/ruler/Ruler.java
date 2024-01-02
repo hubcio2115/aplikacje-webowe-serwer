217cc8873557c4957ae1d8531e3b118f9dab739d
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-02 17:57:26 +0100

    updated ruler serialization

--- src/main/java/org/example/server/domains/ruler/Ruler.java
+++ src/main/java/org/example/server/domains/ruler/Ruler.java
@@ -1,30 +1,35 @@
 package org.example.server.domains.ruler;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
 import lombok.*;
 import org.example.server.domains.country.Country;
 
-import java.io.Serializable;
-import java.sql.Date;
-
-@Data
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table
+@JsonSerialize
 public class Ruler implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Getter
   private Long id;
 
   @Column(name = "name")
+  @Getter
   private String name;
 
   @Column(name = "surname")
+  @Getter
   private String surname;
 
   @Column(name = "office_start_date")
+  @Getter
   private Date officeStartDate;
 
   @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
