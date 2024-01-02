ed890c9246e6afa816becf2a5a84b8d1ed6e7b80
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-02 09:44:34 +0100

    added user management

---
+++ src/main/java/org/example/server/domains/user/User.java
@@ -0,0 +1,71 @@
+package org.example.server.domains.user;
+
+import jakarta.persistence.*;
+import org.example.server.domains.token.Token;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+  @Id @GeneratedValue private Integer id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private String email;
+  private String password;
+
+  @OneToMany(mappedBy = "user")
+  private List<Token> tokens;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
